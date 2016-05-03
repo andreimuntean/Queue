0ad8c3afde554f6e79ce9ab35abf9ef0e3c97229
andrei.muntean.dev@gmail.com <andrei.muntean.dev@gmail.com>
2016-05-03 18:35:17 +0100

    Can now add and remove elements

--- queue.go
+++ queue.go
@@ -6,6 +6,9 @@ import (
 
 type Queue struct {
 	values []interface{}
+	nextPopIndex int
+	nextPushIndex int
+	length int
 }
 
 func New(capacity int) (*Queue, error) {
@@ -17,4 +20,55 @@ func New(capacity int) (*Queue, error) {
 	queue.values = make([]interface{}, capacity)
 
 	return queue, nil
+}
+
+func (queue *Queue) Cap() int {
+	return cap(queue.values)
+}
+
+func (queue *Queue) Len() int {
+	return queue.length	
+}
+
+func (queue *Queue) IsEmpty() bool {
+	return queue.Len() == 0
+}
+
+func (queue *Queue) IsFull() bool {
+	return queue.Len() == queue.Cap()
+}
+
+func (queue *Queue) Push(values ...interface{}) {
+	for _, value := range values {
+		if queue.IsFull() {
+			// Does not accept new values.
+			return
+		}
+
+		// Adds the value to the queue.
+		queue.values[queue.nextPushIndex] = value
+		queue.nextPushIndex = (queue.nextPushIndex + 1) % len(queue.values)
+		queue.length++
+	}
+}
+
+func (queue *Queue) Peek() interface{} {
+	if queue.IsEmpty() {
+		return nil
+	}
+
+	return queue.values[queue.nextPopIndex]
+}
+
+func (queue *Queue) Pop() interface{} {
+	if queue.IsEmpty() {
+		return nil
+	}
+
+	// Removes the value from the queue.
+	value := queue.values[queue.nextPopIndex]
+	queue.nextPopIndex = (queue.nextPopIndex + 1) % len(queue.values)
+	queue.length--
+
+	return value
 }
--- queue_test.go
+++ queue_test.go
@@ -10,14 +10,81 @@ func TestNewQueue(t *testing.T) {
 	assert := assert.New(t)
 
 	_, firstErr := queue.New(0);
-	assert.NotNil(firstErr, "Does not accept non-positive capacity.")
-
 	_, secondErr := queue.New(1);
-	assert.Nil(secondErr, "Can create queue with 1 element.")
-
 	_, thirdErr := queue.New(5);
-	assert.Nil(thirdErr, "Can create queue with 5 elements.")
-
 	_, fourthErr := queue.New(1000000);
+
+	assert.NotNil(firstErr, "Does not accept non-positive capacity.")
+	assert.Nil(secondErr, "Can create queue with 1 element.")
+	assert.Nil(thirdErr, "Can create queue with 5 elements.")
 	assert.Nil(fourthErr, "Can create queue with 1000000 elements.")
+}
+
+func TestCap(t *testing.T) {
+	assert := assert.New(t)
+
+	firstQueue, _ := queue.New(1);
+	secondQueue, _ := queue.New(5);
+	thirdQueue, _ := queue.New(10);
+	fourthQueue, _ := queue.New(100);
+
+	assert.Equal(1, firstQueue.Cap(), "Returns the correct capacity.")
+	assert.Equal(5, secondQueue.Cap(), "Returns the correct capacity.")
+	assert.Equal(10, thirdQueue.Cap(), "Returns the correct capacity.")
+	assert.Equal(100, fourthQueue.Cap(), "Returns the correct capacity.")
+}
+
+func TestLen(t *testing.T) {
+	assert := assert.New(t)
+
+	queue, _ := queue.New(5);
+	assert.Equal(queue.Len(), 0, "Returns the correct length.")
+
+	queue.Push(0)
+	assert.Equal(queue.Len(), 1, "Returns the correct length.")
+
+	queue.Push(0, 0, 0, 0)
+	assert.Equal(queue.Len(), 5, "Returns the correct length.")
+
+	queue.Push(0, 0, 0)
+	queue.Pop()
+	assert.Equal(queue.Len(), 4, "Returns the correct length.")
+
+	queue.Pop()
+	queue.Pop()
+	queue.Pop()
+	queue.Pop()
+	queue.Push(0)
+	assert.Equal(queue.Len(), 1, "Returns the correct length.")
+
+	queue.Pop()
+	assert.Equal(queue.Len(), 0, "Returns the correct length.")
+
+	queue.Pop()
+	assert.Equal(queue.Len(), 0, "Returns the correct length.")
+}
+
+func TestPush(t *testing.T) {
+	assert := assert.New(t)
+
+	queue, _ := queue.New(5)
+
+	queue.Push(10)
+	assert.Equal(1, queue.Len(), "Push enqueues a value.")
+	assert.Equal(10, queue.Peek(), "Push enqueues a value.")
+
+	queue.Push(20)
+	assert.Equal(2, queue.Len(), "Push enqueues a value.")
+	assert.Equal(10, queue.Peek(), "Push enqueues a value.")
+	
+	queue.Push(30, 40, 50)
+	assert.Equal(5, queue.Len(), "Push enqueues multiple values.")
+	
+	queue.Push(99)
+	assert.Equal(5, queue.Len(), "Push no longer adds values when the queue is full.")
+
+	for _, expectedValue := range []int { 10, 20, 30, 40, 50 } {
+		actualValue := queue.Pop()
+		assert.Equal(expectedValue, actualValue, "Push enqueues a value.")
+	}
 }
